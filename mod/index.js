dd183d31f5559168105f1c4fdc29dea8dfe228be
Climax708 <soryy708@gmail.com>
2020-12-20 15:33:56 +0200

    Fix integer test doesnt work

--- mod/index.js
+++ mod/index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
-    const isInteger = num => num === 10 * Math.floor(num / 10);
+    const isInteger = num => num === (Math.floor(num * 10) / 10);
     if (req.query.a === undefined || req.query.b === undefined ||
         req.query.a === null || req.query.b === null ||
         isNaN(Number(req.query.a)) || isNaN(Number(req.query.b)) ||
